1ceb8e570c680a3c481be47c6e32f299bb5bb4c5
Alexandre FRASCHINI <alexandre.fraschini@gmail.com>
2020-02-18 12:30:32 +0100

    basis for spaceships index page + show page

--- app/models/spaceship.rb
+++ app/models/spaceship.rb
@@ -1,6 +1,6 @@
 class Spaceship < ApplicationRecord
   # TODO : remove the 'optional: true' from 'belongs_to'
-  belongs_to :user, optional: true
+  belongs_to :user, dependent: :destroy, optional: true
   has_many :bookings
   has_many :reviews
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-
 require 'faker'
 
 puts "Cleaning database..."
@@ -17,7 +9,7 @@ puts "Creating spaceships..."
     name: Faker::Movies::StarWars.vehicle,
     address: Faker::Movies::StarWars.planet,
     description: Faker::Movies::StarWars.wookiee_sentence,
-    main_pic_url: Faker::LoremFlickr.image(size: "60x50", search_terms: ['vehicle']),
+    main_pic_url: "https://source.unsplash.com/400x320/?aircraft,fighter",
     unit_price: rand(1..150) * 1000,
     size: rand(10..1000),
     max_speed: rand(5..50),
