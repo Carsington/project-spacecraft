8d6ce8f1ee78d203af8d38b05e82357b926045a4
Pierre-Loïc Pichon <pierreloic.pichon@gmail.com>
2020-02-18 16:20:35 +0100

    fixed seeding and models

--- app/models/spaceship.rb
+++ app/models/spaceship.rb
@@ -1,6 +1,5 @@
 class Spaceship < ApplicationRecord
-  # TODO : remove the 'optional: true' from 'belongs_to'
   belongs_to :user
-  has_many :bookings
+  has_many :bookings, dependent: :destroy
   has_many :reviews
 end
