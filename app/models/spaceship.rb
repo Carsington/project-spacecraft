018e72042c180927fc4f23441993e4d766dd8166
Pierre-Loïc Pichon <pierreloic.pichon@gmail.com>
2020-02-18 16:05:24 +0100

    removed destroy dependent and optional in spaceship model

--- app/models/spaceship.rb
+++ app/models/spaceship.rb
@@ -1,6 +1,6 @@
 class Spaceship < ApplicationRecord
   # TODO : remove the 'optional: true' from 'belongs_to'
-  belongs_to :user, dependent: :destroy, optional: true
+  belongs_to :user
   has_many :bookings
   has_many :reviews
 end
