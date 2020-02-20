a87c84751e16958bceaa6e2cc009c39db59d4af5
Pierre-Loïc Pichon <pierreloic.pichon@gmail.com>
2020-02-20 16:48:59 +0100

    spaceSHIIIIP

--- app/models/spaceship.rb
+++ app/models/spaceship.rb
@@ -1,4 +1,11 @@
 class Spaceship < ApplicationRecord
+  include PgSearch::Model
+  pg_search_scope :search,
+    against: [ :user, :name, :address, :description ],
+    using: {
+      tsearch: { prefix: true }
+    }
+
   belongs_to :user
   has_many :bookings, dependent: :destroy
   has_many :reviews
@@ -7,5 +14,4 @@ class Spaceship < ApplicationRecord
             :max_speed, :capacity, presence: true
   validates :name, uniqueness: true, length: { minimum: 5 }
   validates :description, length: { minimum: 20 }
-  # validates :main_pic_url, presence: true
 end
